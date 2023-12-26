aff81b9df811567df1c235f3186ae5a941475746
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-12-26 16:11:48 +0300

    Замечания доработаны

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,21 +2,25 @@ describe('template spec', () => {
 
   beforeEach( () => {
     cy.visit('/')
-
   })
 
+  const {
+    email,
+    pass,
+  } = require("../fixtures/all.json");
+
   it ('test login', () => {
-    cy.login('bropet@mail.ru','123')
+    cy.login(email,pass)
     cy.contains('Добро пожаловать bropet@mail.ru').should('be.visible')
   })
 
   it ('test empty email', () => {
-    cy.login(null,'123')
+    cy.login(null,pass)
     cy.contains('have fun!').should('be.visible')
   })
 
   it ('test empty password', () => {
-    cy.login('bropet@mail.ru', null)
+    cy.login(email, null)
     cy.get('#pass').then((elements) => {
       expect(elements[0].checkValidity()).to.be.false
       expect(elements[0].validationMessage).to.be.eql('Заполните это поле.')
@@ -24,14 +28,14 @@ describe('template spec', () => {
   })
 
   it ('test the window add new book', () => {
-    cy.login('bropet@mail.ru','123')
+    cy.login(email,pass)
     cy.contains('Добро пожаловать bropet@mail.ru').should('be.visible') 
     cy.get('.p-0 > .btn').click()
     cy.contains('Book description').should('be.visible') 
   })
 
   it('test add new book', () => {
-    cy.login('bropet@mail.ru','123')
+    cy.login(email,pass)
     cy.contains('Добро пожаловать bropet@mail.ru').should('be.visible') 
     cy.get('.p-0 > .btn').click()
     cy.contains('Book description').should('be.visible') 
@@ -43,10 +47,11 @@ describe('template spec', () => {
     cy.get('form > .ml-2').click()
   })
 
-  it ('test delite from favorite', () => {
-    cy.login('bropet@mail.ru','123')
-    cy.get('[href="book/cceec804-6561-4e75-9915-8d4ac309228b"] > .h-100 > .card-body')
-    cy.get('[href="book/cceec804-6561-4e75-9915-8d4ac309228b"] > .h-100 > .card-footer > .btn').click()
-    cy.contains('Add to favorite').should('be.visible')
-  })
+  it ('Delete and add favorite', () => {
+    cy.login(email,pass)
+    cy.get('.d-flex')
+    cy.get('[href="book/6a0d3727-318b-4c9b-a13c-cbf13526824a"] > .h-100')
+    cy.get('[href="book/6a0d3727-318b-4c9b-a13c-cbf13526824a"] > .h-100 > .card-footer > .btn').click()
+    cy.get('[href="book/6a0d3727-318b-4c9b-a13c-cbf13526824a"] > .h-100 > .card-footer > .btn').click()
+  });
 })
